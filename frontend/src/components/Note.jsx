c011a067968d66e53c9f363f82cbc43cb3bf4be3
Durgaprasad-54 <rdkgamers555@gmail.com>
2025-06-12 10:55:31 +0530

    add css

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,27 +1,29 @@
-import './Note.css'
-import {useState,useEffect} from 'react'
-import {Link,useNavigate} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Note = () => {
   const navigate = useNavigate();
-  const Token=localStorage.getItem('Token')
-  const [notes,setNotes] = useState([])
-  useEffect(()=>{
-    fetch('http://127.0.0.1:8000/note',{
-      headers:{
-        "Authorization":`Bearer ${Token}`
+  const Token = localStorage.getItem('Token')
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/note', {
+      headers: {
+        "Authorization": `Bearer ${Token}`
       }
-    }).then((res)=>{
+    }).then((res) => {
       return res.json()
-    }).then((data)=>{
+    }).then((data) => {
       setNotes(data)
-    }).catch((err)=>{
+    }).catch((err) => {
       setNotes([])
     })
-  },[Token])
-  if(!Token){
+  }, [Token])
+
+  if (!Token) {
     navigate('/login')
   }
+
   const handledel = (id) => {
     fetch(`http://127.0.0.1:8000/deletenote/${id}`, {
       method: 'DELETE',
@@ -41,25 +43,25 @@ const Note = () => {
 
   return (
     <div className='main'>
-        <div className='header'>
-            <h1>Note App</h1>
-            <h1><Link to={'/profile'}>Profile</Link></h1>
-        </div>
-         {notes.map((note, index) => (
-          <div key={index} className="note-card">
-            <h3>{note.title}</h3>
-            <p>{note.content}</p>
-            <button onClick={()=>{handledel(note._id)}}>delete</button>
+      <div className='header'>
+        <h1>Note App</h1>
+        <h1><Link to={'/profile'}>Profile</Link></h1>
+      </div>
+
+      {notes.map((note, index) => (
+        <div key={index} className="note-card">
+          <h3>{note.title}</h3>
+          <p>{note.content}</p>
+          <div className="note-actions">
+            <button onClick={() => handledel(note._id)}>delete</button>
             <button onClick={() => navigate(`/updatenote/${note._id}`)}>edit</button>
-
           </div>
-        
-        ))}
-        <div>
-          <h3><Link to={'/createnote'}>New Note</Link></h3>
         </div>
+      ))}
 
-
+      <div>
+        <h3 className='new-note-link'><Link to={'/createnote'}>New Note</Link></h3>
+      </div>
     </div>
   )
 }
